9ef7ff935c2e3c46e93f2bf9ec5ea47712394be6
Kfir <kfir.erlich28@gmail.com>
2023-05-19 12:54:34 +0300

    added new pages in react

---
+++ sikumonet-app/src/JavaScript/mainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+        <div>
+            <h1>Welcome to the Main Page!</h1>
+            {/* Add your main page content here */}
+        </div>
+    );
+}
+
+export default MainPage;
